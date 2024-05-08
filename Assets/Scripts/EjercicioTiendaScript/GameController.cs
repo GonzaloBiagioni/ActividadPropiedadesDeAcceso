3103d3a533aa8ca21953f0f86cac5683d27f90e8
GonzaloBiagioni <gonzalobiagioni3@gmail.com>
2024-05-08 16:56:21 -0300

    modificado un script

--- Assets/Scripts/EjercicioTiendaScript/GameController.cs
+++ Assets/Scripts/EjercicioTiendaScript/GameController.cs
@@ -2,21 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Tienda
-{
-    // propiedad de acceso automatico para nombre y precio
-    public string nombre { get; set; }
-    public float precio { get; set; }
-}
-
 public class GameController : MonoBehaviour
 {
     void Start()
     {
         // crea instancia de la clase tienda
-        Tienda espadaDeHierro = new Tienda {nombre = "Espada de Hierro", precio = 1.5f};
-        Tienda escudoDeMadera = new Tienda {nombre = "Escudo de Madera", precio = 0.5f} ;
-        Tienda pistolaPewPew = new Tienda {nombre = "Pistola:Pew-Pew", precio = 9999f};
+        Tienda espadaDeHierro = new () {nombre = "Espada de Hierro", precio = 1.5f};
+        Tienda escudoDeMadera = new () {nombre = "Escudo de Madera", precio = 0.5f} ;
+        Tienda pistolaPewPew = new () {nombre = "Pistola:Pew-Pew", precio = 9999f};
 
         // muestra informacion en la consola
         Debug.Log("Producto: " + espadaDeHierro.nombre + ", Precio: " + espadaDeHierro.precio + " unidades monetarias");
---
+++ Assets/Scripts/EjercicioTiendaScript/Tienda.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tienda
+{
+    // propiedad de acceso automatico para nombre y precio
+    public string nombre { get; set; }
+    public float precio { get; set; }
+
+}
